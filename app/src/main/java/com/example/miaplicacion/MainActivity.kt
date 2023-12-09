922115cffad05c1894ecca8705251b20edbc52fb
cristianbrunone <cristianbrunonecmbc@gmail.com>
2023-12-09 17:24:37 -0300

    include print

--- app/src/main/java/com/example/miaplicacion/MainActivity.kt
+++ app/src/main/java/com/example/miaplicacion/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
+                    print("esto es un ejemplo")
                 }
             }
         }
