91e982c028fd410cc9195cae7a56f46c9a932695
CristianBrunone <140309792+cristianbrunone@users.noreply.github.com>
2024-03-29 21:46:59 -0300

    Delete RedSocial directory

--- RedSocial/app/src/main/java/com/example/myapplication/JetpackComposeApp.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-
-@HiltAndroidApp
-class JetpackComposeApp: Application() {
-}
--- RedSocial/app/src/main/java/com/example/myapplication/login/data/network/response/LoginResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication.login.data.network.response
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginResponse(@SerializedName("success")val success:Boolean)
