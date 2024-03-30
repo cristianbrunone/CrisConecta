e5624586c15b777c7dad337d606912311f5f7ad0
CristianBrunone <140309792+cristianbrunone@users.noreply.github.com>
2024-03-29 21:48:14 -0300

    Delete BrunonePads directory

--- BrunonePads/app/src/main/java/com/example/brunonepads/model/SuperNota.kt
+++
@@ -1,8 +0,0 @@
-package com.example.brunonepads.model
-
-import androidx.annotation.DrawableRes
-
-data class SuperNota(var notaName: String,
-                     @DrawableRes var photo: Int,
-                     var soundFilePath: String = "Pad_em_C.mp3"
-)
